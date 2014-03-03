d5ec9ede8b5856708e684e874365683126157ab4
Qing, Lv <luqin@airbiquity.com>
2014-03-03 10:10:07 +0800

    add new games

---
+++ BiYanLi1_/js/main.js
@@ -0,0 +1,207 @@
+﻿init(100,"mylegend",525,500,main);
+LSystem.screen(LStage.FULL_SCREEN);
+var backLayer,tileLayer,ctrlLayer,overLayer,gameoverLayer,selectLayer;
+var tileText,overText,gameoverText;
+var col,row;
+var time = 0;
+var checkpoints = [
+	["籍","藉"],
+	["我","找"],
+	["春","舂"],
+	["龙","尤"],
+	["曰","日"]
+];
+var checkpointNo = 0;
+var i0;
+var j0;
+var i,j;
+var partX,partY;
+var overTextContent = ["恭喜您，您过关了","进入下一关","重新开始"];
+var gameoverTextContent = ["对不起，您失败了","重开关卡"];
+var nowLine;
+var setTimeLine;
+function main(){
+	i0 = Math.floor(Math.random()*10);
+	j0 = Math.floor(Math.random()*10);
+
+	initLayer();
+	initCtrl();
+	initTile();
+}
+function initLayer(){
+	backLayer = new LSprite();
+	addChild(backLayer);
+	
+	tileLayer = new LSprite();
+	backLayer.addChild(tileLayer);
+
+	ctrlLayer = new LSprite();
+	backLayer.addChild(ctrlLayer);
+}
+function initCtrl(){
+	col = 10;
+	row = 10;
+	addEvent();
+	addTimeLine();
+}
+function initTile(){
+	for(i=0;i<row;i++){
+		for(j=0;j<col;j++){
+			tile();
+		}
+	}
+}
+function tile(){
+	tileLayer.graphics.drawRect(3,"dimgray",[j*50,i*50,50,50],true,"lightgray");
+
+	var w = checkpoints[checkpointNo][(i==i0 && j==j0) ? 0 : 1];
+	tileText = new LTextField();
+	tileText.weight = "bold";
+	tileText.text = w;
+	tileText.size = 25;
+  	tileText.color = "lightgray";
+	tileText.font = "黑体";
+	tileText.x = j*50+7;
+	tileText.y = i*50+7;
+	tileLayer.addChild(tileText);
+	
+	var shadow = new LDropShadowFilter(1,15,"#000000");
+	tileLayer.filters = [shadow];
+}
+function addEvent(event){
+	overLayer = new LSprite();
+	backLayer.addChild(overLayer);
+
+	selectLayer = new LSprite();
+	backLayer.addChild(selectLayer);
+
+	gameoverLayer = new LSprite();
+	backLayer.addChild(gameoverLayer);
+
+	tileLayer.addEventListener(LMouseEvent.MOUSE_DOWN,onDown);
+	selectLayer.addEventListener(LMouseEvent.MOUSE_UP,gameReStart);
+	gameoverLayer.addEventListener(LMouseEvent.MOUSE_UP,reTry);
+}
+function gameReStart(){
+	i0 = Math.floor(Math.random()*10);
+	j0 = Math.floor(Math.random()*10);
+
+	time = 0;
+
+	tileLayer.removeAllChild();
+	overLayer.removeAllChild();
+	selectLayer.removeAllChild();
+	backLayer.removeChild(selectLayer);
+	backLayer.removeChild(overLayer);
+	if(checkpointNo != checkpoints.length-1){
+		checkpointNo++;
+	}
+	initTile();
+	addEvent();
+	addTimeLine();
+}
+function reTry(){
+	i0 = Math.floor(Math.random()*10);
+	j0 = Math.floor(Math.random()*10);
+
+	time = 0;
+
+	tileLayer.removeAllChild();
+	overLayer.removeAllChild();
+	gameoverLayer.removeAllChild();
+	selectLayer.removeAllChild();
+	backLayer.removeChild(selectLayer);
+	backLayer.removeChild(overLayer);
+	backLayer.removeChild(gameoverLayer);
+
+	initTile();
+	addEvent();
+	addTimeLine();
+}
+function addTimeLine(){
+	overLayer.graphics.drawRect(5,"dimgray",[500,0,20,500],true,"lightgray");
+	overLayer.graphics.drawLine(15,"lightgray",[510,3,510,497]);
+	overLayer.graphics.drawLine(15,"red",[510,3,510,497]);
+	setTimeLine = setInterval(function(){drawTimeLine();},100);
+}
+function drawTimeLine(){
+	nowLine = 3+((time/5)*495)/10;
+	overLayer.graphics.drawLine(15,"lightgray",[510,3,510,497]);
+	overLayer.graphics.drawLine(15,"red",[510,nowLine,510,497]);
+	time++;
+	if(time>50){
+		clearInterval(setTimeLine);
+		gameOver();
+	}
+}
+function gameOver(){
+	overLayer.graphics.drawRect(5,"dimgray",[(LGlobal.width - 420)*0.5,80,420,250],true,"lightgray");
+	gameoverLayer.graphics.drawRect(5,"dimgray",[(LGlobal.width - 250)*0.5,230,250,50],true,"darkgray");
+
+	for(var i=0;i<gameoverTextContent.length;i++){
+		gameoverText = new LTextField();
+		gameoverText.weight = "bold";
+		gameoverText.color = "dimgray";
+		gameoverText.font = "黑体";
+		if(i==0){
+			gameoverText.text = gameoverTextContent[i];
+			gameoverText.size = 35;
+			gameoverText.x = (LGlobal.width - gameoverText.getWidth())*0.5;
+			gameoverText.y = 120;
+			gameoverLayer.addChild(gameoverText);
+		}else if(i==1){
+			gameoverText.text = gameoverTextContent[i];
+			gameoverText.size = 20;
+			gameoverText.x = (LGlobal.width - gameoverText.getWidth())*0.5;
+			gameoverText.y = 240;
+			gameoverLayer.addChild(gameoverText);
+		}
+	}
+	tileLayer.removeEventListener(LMouseEvent.MOUSE_DOWN,onDown);
+}
+function onDown(event){
+	var mouseX,mouseY;
+	mouseX = event.offsetX;
+	mouseY = event.offsetY;
+
+	partX = Math.floor((mouseX)/50);
+	partY = Math.floor((mouseY)/50);
+	isTure(partX,partY);
+}
+function isTure(x,y){
+	if(x==j0 && y==i0){
+		clearInterval(setTimeLine);
+		overLayer.graphics.drawRect(5,"dimgray",[(LGlobal.width - 420)*0.5,80,420,250],true,"lightgray");
+		selectLayer.graphics.drawRect(5,"dimgray",[(LGlobal.width - 250)*0.5,230,250,50],true,"darkgray");
+
+		for(var i=0;i<overTextContent.length;i++){
+			overText = new LTextField();
+			overText.weight = "bold";
+			overText.color = "dimgray";
+			overText.font = "黑体";
+			if(i==0){
+				overText.text = overTextContent[i];
+				overText.size = 35;
+				overText.x = (LGlobal.width - overText.getWidth())*0.5;
+				overText.y = 120;
+				overLayer.addChild(overText);
+			}else if(i==1){
+				if(checkpointNo == checkpoints.length-1){
+					overText.text = overTextContent[i+1];
+					overText.size = 20;
+					overText.x = (LGlobal.width - overText.getWidth())*0.5;
+					overText.y = 240; 
+					selectLayer.addChild(overText);
+					checkpointNo = 0;
+				}else{
+					overText.text = overTextContent[i];
+					overText.size = 20;
+					overText.x = (LGlobal.width - overText.getWidth())*0.5;
+					overText.y = 240;
+					selectLayer.addChild(overText);
+				}
+			}
+		}
+		tileLayer.removeEventListener(LMouseEvent.MOUSE_DOWN,onDown);
+	}
+}
---
+++ Qi_TicTacToe_/js/main.js
@@ -0,0 +1,216 @@
+﻿init(30,"mylegend",390,420,main);
+var backLayer,chessLayer,overLayer;
+var statusText = new LTextField();
+var statusContent="您先请吧……";
+var matrix = [
+	[0,0,0],
+	[0,0,0],
+	[0,0,0]
+];
+var usersTurn = true;
+var step = 0;
+var title = "井字棋";
+var introduction = ""
+var infoArr = [title,introduction];
+
+function main(){
+	gameInit();
+	addText();
+	addLattice();	
+}
+function gameInit(){
+	initLayer();
+	addEvent();
+}
+function initLayer(){
+	backLayer = new LSprite();
+	addChild(backLayer);
+
+	chessLayer = new LSprite();
+	backLayer.addChild(chessLayer);
+
+	overLayer = new LSprite();
+	backLayer.addChild(overLayer);
+}
+function addEvent(){
+	backLayer.addEventListener(LMouseEvent.MOUSE_DOWN,onDown);
+}
+function onDown(){
+	var mouseX,mouseY;
+	mouseX = event.offsetX;
+	mouseY = event.offsetY;
+
+	var partX = Math.floor(mouseX/130);
+	var partY = Math.floor(mouseY/130);
+	if(matrix[partX][partY]==0){
+		usersTurn=false;
+		matrix[partX][partY]=-1;
+		step++;
+		update(partX,partY);
+		
+		if(win(partX,partY)){
+			statusContent = "帅呆了，你赢啦！点击屏幕重开游戏。";
+			gameover();
+			addText();
+		}else if(isEnd()){
+			statusContent = "平局啦~~点击屏幕重开游戏。";
+			gameover();
+			addText();
+		}else{
+			statusContent = "电脑正在思考中……";
+			addText();
+			computerThink();
+		}
+	}
+}
+function addText(){
+	statusText.size = 15;	
+	statusText.weight = "bold";
+	statusText.color = "white";
+	statusText.text = statusContent;
+	statusText.x = (LGlobal.width-statusText.getWidth())*0.5;
+	statusText.y = 393;
+	
+	overLayer.addChild(statusText);
+}
+function addLattice(){
+	backLayer.graphics.drawRect(10,"dimgray",[0,0,390,420],true,"dimgray");
+	backLayer.graphics.drawRect(10,"dimgray",[0,0,390,390],true,"lavender");
+	for(var i=0;i<3;i++){
+		backLayer.graphics.drawLine(3,"dimgray",[130*i,0,130*i,390]);
+	}
+	for(var i=0;i<3;i++){
+		backLayer.graphics.drawLine(3,"dimgray",[0,130*i,390,130*i]);
+	}
+}
+function update(x,y){
+	var v = matrix[x][y];
+	if(v>0){
+		chessLayer.graphics.drawArc(10,"green",[x*130+65,y*130+65,40,0,2*Math.PI]);
+	}else if(v<0){
+		chessLayer.graphics.drawLine(20,"#CC0000",[130*x+30,130*y+30,130*(x+1)-30,130*(y+1)-30]);
+		chessLayer.graphics.drawLine(20,"#CC0000",[130*(x+1)-30,130*y+30,130*x+30,130*(y+1)-30]);
+	}
+}
+function computerThink(){
+	var b = best();
+	var x = b.x;
+	var y = b.y;
+	matrix[x][y]=1;
+	step++;
+	update(x,y);
+	
+	if(win(x,y)){
+		statusContent = "哈哈你输了！点击屏幕重开游戏。";
+		gameover();
+		addText();
+	}else if(isEnd()){
+		statusContent = "平局啦~~点击屏幕重开游戏。";
+		gameover();
+		addText();
+	}else{
+		statusContent = "该你了！！！";
+		addText();
+	}
+}
+function isEnd(){
+	return step>=9;
+}
+function win(x,y){
+	if(Math.abs(matrix[x][0]+matrix[x][1]+matrix[x][2])==3){
+		return true;
+	}
+	if(Math.abs(matrix[0][y]+matrix[1][y]+matrix[2][y])==3){
+		return true;
+	}
+	if(Math.abs(matrix[0][0]+matrix[1][1]+matrix[2][2])==3){
+		return true;
+	}
+	if(Math.abs(matrix[2][0]+matrix[1][1]+matrix[0][2])==3){
+		return true;
+	}
+	return false;
+}
+function best(){
+	var bestx;
+	var besty;
+	var bestv=0;
+	for(var x=0;x<3;x++){
+		for(var y=0;y<3;y++){
+			if(matrix[x][y]==0){
+				matrix[x][y] = 1;
+				step++;
+				if(win(x,y)){
+					step--;
+					matrix[x][y] = 0;	
+					return {'x':x,'y':y,'v':1000};
+				}else if(isEnd()){
+					step--;
+					matrix[x][y]=0;	
+					return {'x':x,'y':y,'v':0};
+				}else{
+					var v=worst().v;
+					step--;
+					matrix[x][y]=0;
+					if(bestx==null || v>=bestv){
+						bestx=x;
+						besty=y;
+						bestv=v;
+					}
+				}
+			}
+		}
+	}
+	return {'x':bestx,'y':besty,'v':bestv};
+}
+function worst(){
+	var bestx;
+	var besty;
+	var bestv = 0;
+	for(var x=0;x<3;x++){
+		for(var y=0;y<3;y++){
+			if(matrix[x][y] == 0){
+				matrix[x][y] = -1;
+				step++;
+				if(win(x,y)){
+					step--;
+					matrix[x][y] = 0;	
+					return {'x':x,'y':y,'v':-1000};
+				}else if(isEnd()){
+					step--;
+					matrix[x][y]=0;	
+					return {'x':x,'y':y,'v':0};;
+				}else{
+					var v=best().v;
+					step--;
+					matrix[x][y]=0;
+					if(bestx==null || v<=bestv){
+						bestx=x;
+						besty=y;
+						bestv=v;
+					}
+				}
+				
+			}
+		}
+	}
+	return {'x':bestx,'y':besty,'v':bestv};
+}
+function gameover(){
+	backLayer.removeEventListener(LMouseEvent.MOUSE_DOWN,onDown);
+	backLayer.addEventListener(LMouseEvent.MOUSE_DOWN,function(){
+		chessLayer.removeAllChild();
+		backLayer.removeChild(chessLayer);
+		backLayer.removeChild(overLayer);
+		removeChild(backLayer);
+		matrix = [
+			[0,0,0],
+			[0,0,0],
+			[0,0,0]
+		];
+		step = 0;
+		main();
+		statusContent = "您先请吧……";
+		addText();
+	});
+}
